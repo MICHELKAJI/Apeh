439b8dc0962e5235ba2489b01f10bc7508574a7c
Michel <fistonkajibwami2@gmail.com>
2024-09-15 03:02:24 -0330

    change background for banner

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigateToDirective } from '../navigate-to.directive';
+import { CarousolComponent } from '../carousol/carousol.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavigateToDirective],
+  imports: [NavigateToDirective, CarousolComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CarousolComponent } from '../carousol/carousol.component';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [CarousolComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
