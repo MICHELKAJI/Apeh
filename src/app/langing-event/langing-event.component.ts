2d72edbd036a203652fb164008697ba69b5cbdb0
Michel <fistonkajibwami2@gmail.com>
2024-11-13 11:29:00 -0330

    fixed background video

--- src/app/langing-event/langing-event.component.ts
+++ src/app/langing-event/langing-event.component.ts
@@ -1,11 +1,12 @@
 import { parseTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { PageCausesComponent } from '../page-causes/page-causes.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-langing-event',
   standalone: true,
-  imports: [ PageCausesComponent],
+  imports: [ PageCausesComponent, FooterComponent],
   templateUrl: './langing-event.component.html',
   styleUrl: './langing-event.component.css'
 })
