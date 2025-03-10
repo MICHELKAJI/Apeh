5f15ae5b78a8c44e6586f33eedc1d2b2cd91ada7
Michel <fistonkajibwami2@gmail.com>
2025-03-09 22:50:14 -0330

    fixe new style

--- src/app/make-action/make-action.component.ts
+++ src/app/make-action/make-action.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DoDonationService, Data } from '../do-donation.service';
@@ -6,69 +6,74 @@ import { DoDonationService, Data } from '../do-donation.service';
 @Component({
   selector: 'app-make-action',
   standalone: true,
-  imports: [ NgClass, FormsModule],
+  imports: [ NgClass, FormsModule, NgIf],
   templateUrl: './make-action.component.html',
   styleUrl: './make-action.component.css'
 })
 export class MakeActionComponent {
   nom: string = '';
   tel: string = '';
-  ville: string = ''
-  mail: string = '';  // Propriété liée à l'input
-  message: string = ''; // Pour afficher un message de succès ou d'erreur
-  messageType : string='';
+  ville: string = '';
+  mail: string = '';
+  message: string = '';
+  messageType: string = '';
+  isLoading: boolean = false; // 👈 Nouveau !
 
-  constructor( private doDonationService : DoDonationService){}
+  constructor(private doDonationService: DoDonationService) {}
 
-  submitNewDonation() : void {
+  submitNewDonation(): void {
     const nomRegex = /^.{3,30}$/;
     const telRegex = /^\+?[1-9]\d{1,14}$/;
     const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
     const villeRegex = /^.{3,30}$/;
 
-
-    if(!nomRegex.test(this.nom)){
+    if (!nomRegex.test(this.nom)) {
       this.message = 'Veuillez entrer votre nom.';
+      this.messageType = 'error';
       return;
     }
     if (!emailRegex.test(this.mail)) {
       this.message = 'Veuillez entrer un email valide.';
-      return; // Arrêter l'exécution si l'email est invalide
-    }
-    if(!telRegex.test(this.tel)){
-      this.message = 'Veuillez entrer un bon numero valide.';
+      this.messageType = 'error';
       return;
     }
-    if(!villeRegex.test(this.ville)){
+    if (!telRegex.test(this.tel)) {
+      this.message = 'Veuillez entrer un bon numéro valide.';
+      this.messageType = 'error';
+      return;
+    }
+    if (!villeRegex.test(this.ville)) {
       this.message = 'Veuillez saisir votre ville.';
+      this.messageType = 'error';
       return;
     }
-    
 
-    const data: Data = { nom : this.nom, tel: this.tel, ville: this.ville, mail: this.mail };
+    const data: Data = { nom: this.nom, tel: this.tel, ville: this.ville, mail: this.mail };
+
+    this.isLoading = true; // 👈 Active le loader
 
     this.doDonationService.postDatas(data).subscribe({
       next: (response) => {
-        console.log('Nouveau donateur enregistrer avec succès', response);
-        this.messageType ='succès';
-        this.message = 'Nous sommes ravis de vous avoir parmi nous  !';
-        this.nom = '';
-        this.ville = '';
-        this.tel = '';
-        this.mail = '';
-        
+        console.log('Nouveau donateur enregistré avec succès', response);
+        this.messageType = 'succès';
+        this.message = 'Nous sommes ravis de vous avoir parmi nous !';
+        this.resetForm();
       },
       error: (error) => {
         console.error('Erreur lors de l\'enregistrement.', error);
-        this.messageType ='error';
-        this.message = `Erreur lors de l\'enregistrement vérifier votre connexion`;
-        this.nom = '';
-        this.ville = '';
-        this.tel = '';
-        this.mail = '';
-       
+        this.messageType = 'error';
+        this.message = `Erreur lors de l'enregistrement, vérifiez votre connexion.`;
+        this.resetForm();
       }
     });
   }
 
+  private resetForm() {
+    this.nom = '';
+    this.ville = '';
+    this.tel = '';
+    this.mail = '';
+    this.isLoading = false; // 👈 Désactive le loader
+  }
 }
+
